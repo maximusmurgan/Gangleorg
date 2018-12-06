a867b1b2cbbe5804d810990ebd3f61ad872b78d8
maximusmurgan <32108081+maximusmurgan@users.noreply.github.com>
2018-12-06 09:49:47 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+ $("#about").click(function(){
+    $("#ahh").fadeToggle();
+    $("#OOf").fadeToggle();
+    $("#Text").fadeOut();
+    $("#Text2").fadeOut();
+    $("#Place").fadeToggle();
+    $("#Text3").fadeToggle();
+ });
+ 
+ 
+
+        
+       
+   
+ 
+ 
